d0f42086328617a09dec4a5ac1fa1a50f7447859
Vikas Kumar <95131052+Vikki2021@users.noreply.github.com>
2025-05-01 09:38:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-//complete this code
+// Complete this code
 class Rectangle {
-	//
-	constructor(width, height) {
+  constructor(width, height) {
     if (width <= 0 || height <= 0) {
       throw new Error("Width and height must be positive integers");
     }
@@ -25,20 +24,26 @@ class Rectangle {
   }
 }
 
-class Square extends Animal {
-	//
-	constructor(side) {
+class Square extends Rectangle {
+  constructor(side) {
     if (side <= 0) {
       throw new Error("Side must be a positive integer");
     }
-    super(side, side); // Call Rectangle constructor
+    super(side, side); // Call Rectangle constructor with same side
   }
 
   // Method to calculate perimeter
   getPerimeter() {
-    return 4 * this._width; // or 4 * this.width
+    return 4 * this.width; // or this._width
   }
 }
+
+// const rect = new Rectangle(5, 10);
+// console.log(rect.getArea()); // 50
+
+// const sq = new Square(7);
+// console.log(sq.getArea()); // 49
+// console.log(sq.getPerimeter()); // 28
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
